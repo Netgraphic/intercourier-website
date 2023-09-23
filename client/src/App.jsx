73035dc755f91e27fc069d43c22364b39e431cea
Diego Moris <diego.mt91@gmail.com>
2023-09-23 11:23:02 +0200

    Minor fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
                 setTimeout(() => {
                     setLoadingBg((prev) => ({ ...prev, loader: false }));
                 }, 500);
-            }, 500);
-        }, 2000);
+            }, 1000);
+        }, 1500);
     }, []);
 
     return (
